f7c0b0507a011beca6916b33a6a9cfe85e8dec01
taisei0328 <grape.taichan@gmail.com>
2025-08-31 13:45:59 +0900

    新しい内容の追加

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,16 +1,18 @@
 from flaskr import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import sqlite3
+DATABASE = 'database.db'
 
 @app.route('/')#スラッシュはWebアプリのトップURL(root)
 def index(): #トップ画面にアクセスしたときに実行される
-    books = [
-        {'title': 'はらぺこ',
-        'price': '1200',
-        'arrival_day':'2025年8月30日'
-    },{'title': 'ぐりとぐら',
-        'price': '1000',
-        'arrival_day':'2025年8月25日'
-    },] #リスト　
+    con = sqlite3.connect(DATABASE)
+    db_books = con.execute("SELECT * FROM books").fetchall()
+    con.close()
+
+    books = [] #リスト　
+
+    for row in db_books:
+        books.append({'title': row[0], 'price':row[1], 'arrival_day': row[2]})
 
     return render_template(
         'index.html',
@@ -22,3 +24,20 @@ def form():
     return render_template(
         'form.html'
     )
+
+@app.route('/register', methods=['POST']) #POSTというリクエストの場合に下のregisterという関数が呼び出される
+def register():
+    title=request.form['title']
+    price=request.form['price']
+    arrival_day = request.form['arrival_day']
+
+    con = sqlite3.connect(DATABASE)
+    con.execute('INSERT INTO books VALUES(?,?,?)',
+    [title, price, arrival_day])
+    con.commit()
+    con.close()
+
+    return redirect(url_for('index')) #登録が終わったら自動でトップページに戻る
+
+
+    
