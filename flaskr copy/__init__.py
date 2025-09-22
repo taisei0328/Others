12051dd3e86af206727be190bb5bb0a7c75b3d0c
taisei0328 <grape.taichan@gmail.com>
2025-09-22 14:50:33 +0900

    Delete flaskr copy directory

--- flaskr copy/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__) #__init__.pyを受け取る　__ は半角　この__name__に入るファイルを起点としてつくってとFlaskに指示
-import flaskr.main
-
-from flaskr import db
-db.create_books_table()
-
--- flaskr copy/db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-DATABASE='database.db'
-def create_books_table():
-    con =  sqlite3.connect(DATABASE)#データベースに登録したら自動でIDが付与される、IDを付与することで、登録項目を識別し、それぞれだけを編集できるようにする
-    con.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, price REAL, arrival_day TEXT)")
-    con.close()
