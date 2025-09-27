932cd29cc18640ff5c99d72b65c8dd097c23687a
taisei0328 <grape.taichan@gmail.com>
2025-09-27 21:53:17 +0900

    修正

---
+++ flaskr/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(
+    __name__
+)  # __init__.pyを受け取る　__ は半角　この__name__に入るファイルを起点としてつくってとFlaskに指示
+import flaskr.main
+
+from flaskr import db
+
+db.create_recipes_table()
